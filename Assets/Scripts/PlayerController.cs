3604f5f6565c906d7918df66930cc48dc1cca0de
Dominik Müller <dominimu@stud.ntnu.no>
2026-01-31 12:48:28 +0100

    Added scale to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,10 +19,19 @@ public class PlayerController : MonoBehaviour
     private RaycastHit hitData;
     private LayerMask layerMask;
     private Vector3 offset;
+    private Vector3 playerVelocity;
 
+    private const float GRAVITY = -9.81f;
     private const float DISTANCE = 2.5f;
+    private const float GRAB_POS_X = 0.5f;
+    private const float GRAB_POS_Y = 0.25f;
+    private const float SCALE_SMALL = 0.5f;
+    private const float SCALE_POSITION_OFFSET = 0.5f;
 
     private GameObject holdObject;
+
+    private bool isSmall = false;
+    private bool groundedPlayer;
 
     private void Awake()
     {
@@ -38,14 +47,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer)
+        {
+            playerVelocity.y = 0f;
+        }
+
         Vector3 movement = transform.right * moveVec2.x + transform.forward * moveVec2.y;
+        movement = Vector3.ClampMagnitude(movement, 1f);
+        playerVelocity.y += GRAVITY * Time.deltaTime;
 
-        characterController.Move(movement * speed * Time.deltaTime);
+        Vector3 finalMove = (movement * speed) + (playerVelocity.y * Vector3.up);
+        characterController.Move(finalMove * Time.deltaTime);
 
-        Vector2 velocity = mouseInputVec2 * sensitivity;
+        Vector2 mouseVelocity = mouseInputVec2 * sensitivity;
 
-
-        rotation += velocity * Time.deltaTime;
+        rotation += mouseVelocity * Time.deltaTime;
         rotation.y = Mathf.Clamp(rotation.y, -maxVerticalAngle, maxVerticalAngle);
 
         transform.rotation = Quaternion.Euler(0f, rotation.x, 0f);
@@ -53,7 +70,7 @@ public class PlayerController : MonoBehaviour
 
         if (holdObject != null)
         {
-            holdObject.transform.position = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.25f, DISTANCE));
+            holdObject.transform.position = cam.ViewportToWorldPoint(new Vector3(GRAB_POS_X, GRAB_POS_Y, DISTANCE));
             holdObject.transform.rotation = Quaternion.Euler(-rotation.y, rotation.x, 0f);
             Debug.Log("HoldObject pos: " +  holdObject.transform.position);
         }
@@ -68,13 +85,11 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveVec2 = context.ReadValue<Vector2>();
-        //Debug.Log($"Move Vec3: {moveVec2}");
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
         mouseInputVec2 = context.ReadValue<Vector2>();
-        //Debug.Log($"Look Vec3: {mouseInputVec2}");
     }
 
     public void OnGrab(InputAction.CallbackContext context)
@@ -98,10 +113,31 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
-                holdObject.transform.position = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.25f, DISTANCE));
+                holdObject.transform.position = cam.ViewportToWorldPoint(new Vector3(GRAB_POS_X, GRAB_POS_Y, DISTANCE));
                 holdObject = null;
                 Debug.Log("Hold object should be null: " + holdObject);
             }
+        }
+    }
+
+    public void OnDebugScale(InputAction.CallbackContext context)
+    {
+        if (context.started && context.interaction is PressInteraction)
+        {
+            Debug.Log("DEBUG SCALE function called");
+            if(isSmall)
+            {
+                transform.localScale = Vector3.one;
+                characterController.Move(new Vector3(0.0f, SCALE_POSITION_OFFSET, 0.0f));
+                isSmall = false;
+            }
+            else
+            {
+                transform.localScale = new Vector3(SCALE_SMALL, SCALE_SMALL, SCALE_SMALL);
+                characterController.Move(new Vector3(0.0f, -SCALE_POSITION_OFFSET, 0.0f));
+                isSmall = true;
+            }
+            Debug.Log("Transform pos after scale change: " + transform.position);
         }
 
     }
